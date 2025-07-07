65e09c3f97bfa59a74f228aafec2405b9faee880
Aaro Paltemaa <apaltemaa@gmail.com>
2025-07-07 21:14:52 +0300

    added onmousedown events for canvas

--- apps/frontend/src/components/Canvas/Canvas.tsx
+++ apps/frontend/src/components/Canvas/Canvas.tsx
@@ -1,8 +1,27 @@
 import { Stage, Layer } from "react-konva"
+import type { KonvaEventObject } from "konva/lib/Node";
+import { useState } from "react";
+
+type Line = {
+  points: number[];
+};
 
 const Canvas = () => {
+    const [lines, setLines] = useState<Line[]>([]);
+
+    const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
+        const stage = e.target.getStage();
+        if (!stage) return;
+        const pos = stage.getPointerPosition();
+        if (!pos) return;
+        const newLine: Line = { points: [pos.x, pos.y]}
+        setLines([...lines, newLine])
+        console.log(lines)
+};
+
+
     return (
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Stage onMouseDown={handleMouseDown} width={window.innerWidth} height={window.innerHeight} style={{ background: "#fff" }}>
             <Layer>
             </Layer>
         </Stage>
